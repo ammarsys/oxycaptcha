c3455252674f99edb6f41aa04ba88a163b50ca1e
Ellis <ellispinsky5@gmail.com>
2023-03-14 17:09:33 -0400

    "Convert docstrings from reST to Google style"

--- app.py
+++ app.py
@@ -15,14 +15,7 @@ from typing import Callable
 from io import BytesIO
 
 # Related third party imports
-from flask import (
-    Flask,
-    send_file,
-    render_template,
-    jsonify,
-    redirect,
-    request
-)
+from flask import Flask, send_file, render_template, jsonify, redirect, request
 from flask_cors import CORS
 from PIL import ImageDraw, Image, ImageFont
 import random
@@ -33,25 +26,26 @@ from utils import noise
 
 app = Flask(__name__)
 CORS(app)
-captchas = expiringdict.ExpiringDict(max_age_seconds=120, max_len=float('inf'))
+captchas = expiringdict.ExpiringDict(max_age_seconds=120, max_len=float("inf"))
 
 
 class CaptchaCount:
     """Class to count captchas to prevent duplicates in the captchas dictionary."""
+
     count: int = 0
 
 
 fonts_lower = [
     ImageFont.truetype("./fonts/lower/gadugib.ttf", 32),
     ImageFont.truetype("./fonts/lower/Chalkduster_400.ttf", 32),
-    ImageFont.truetype('./fonts/lower/ShadowsIntoLight-Regular.ttf', 32),
-    ImageFont.truetype('./fonts/lower/Rajdhani-SemiBold.ttf', 32)
+    ImageFont.truetype("./fonts/lower/ShadowsIntoLight-Regular.ttf", 32),
+    ImageFont.truetype("./fonts/lower/Rajdhani-SemiBold.ttf", 32),
 ]
 fonts_upper = [
-    ImageFont.truetype('./fonts/upper/arial.ttf', 55),
-    ImageFont.truetype('./fonts/upper/FallingSky-JKwK.ttf', 55),
-    ImageFont.truetype('./fonts/upper/TrainOne-Regular.ttf', 55),
-    ImageFont.truetype('./fonts/upper/BebasNeue-Regular.ttf', 55)
+    ImageFont.truetype("./fonts/upper/arial.ttf", 55),
+    ImageFont.truetype("./fonts/upper/FallingSky-JKwK.ttf", 55),
+    ImageFont.truetype("./fonts/upper/TrainOne-Regular.ttf", 55),
+    ImageFont.truetype("./fonts/upper/BebasNeue-Regular.ttf", 55),
 ]
 
 
@@ -66,8 +60,8 @@ def id_generator(y: int, choice: Callable) -> str:
     :type choice: Callable
     :return: string in form of an ID
     """
-    string = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKMNOPQRSTUVWXYZ'
-    return ''.join(choice(string) for _ in range(y))
+    string = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKMNOPQRSTUVWXYZ"
+    return "".join(choice(string) for _ in range(y))
 
 
 def cap_gen(text: str) -> Image.Image:
@@ -82,14 +76,12 @@ def cap_gen(text: str) -> Image.Image:
     space, height = random.randint(5, 10), random.randint(5, 10)
 
     corresponding_font = {
-        let: random.choice(
-            fonts_upper if let.isupper() else fonts_lower
-        )
+        let: random.choice(fonts_upper if let.isupper() else fonts_lower)
         for let in text
     }
     text_positions = []
 
-    img = Image.new('RGB', (300, 100), color=(128, 128, 128))
+    img = Image.new("RGB", (300, 100), color=(128, 128, 128))
     img.load()
 
     d = noise.add_noise_lines(ImageDraw.Draw(img))
@@ -101,9 +93,7 @@ def cap_gen(text: str) -> Image.Image:
         space += secrets.choice(range(35, 45))
         height += secrets.choice(range(1, 11))
 
-        text_positions.append(
-            tuple(secrets.randbelow(10) + 15 + i for i in cords)
-        )
+        text_positions.append(tuple(secrets.randbelow(10) + 15 + i for i in cords))
 
     value = secrets.randbelow(len(text_positions))
     for i in range(len(text_positions) - value):
@@ -112,7 +102,7 @@ def cap_gen(text: str) -> Image.Image:
     return img
 
 
-@app.route('/api/cdn/<key>')
+@app.route("/api/cdn/<key>")
 def get_img(key: str):
     """
     CDN (Content Delivery Network) for the captchas.
@@ -133,16 +123,16 @@ def get_img(key: str):
             pil_image = captchas[key][1]
 
         output = BytesIO()
-        pil_image.convert('RGBA').save(output, format='PNG')
+        pil_image.convert("RGBA").save(output, format="PNG")
         output.seek(0, 0)
 
-        return send_file(output, mimetype='image/png', as_attachment=False)
+        return send_file(output, mimetype="image/png", as_attachment=False)
 
     except KeyError:
-        return redirect('/')
+        return redirect("/")
 
 
-@app.route('/api/img')
+@app.route("/api/img")
 def api_captcha():
     """
     The endpoint for creating dictionary key with the captcha ID, it's values being the information about image,
@@ -151,10 +141,10 @@ def api_captcha():
 
     :return: JSON dictionary
     """
-    access = request.args.get('requests', default=10, type=int)
+    access = request.args.get("requests", default=10, type=int)
 
     if access > 20:
-        return redirect('/')
+        return redirect("/")
 
     delta = datetime.timedelta(minutes=5)
     now = datetime.datetime.utcnow()
@@ -163,26 +153,26 @@ def api_captcha():
 
     id_ = base64.b64encode(
         bytes(
-            f'{CaptchaCount.count}.{id_generator(y=10, choice=random.choice)}.{now.strftime("%S")[-5:]}', 'utf-8'
+            f'{CaptchaCount.count}.{id_generator(y=10, choice=random.choice)}.{now.strftime("%S")[-5:]}',
+            "utf-8",
         )
     ).decode()
 
     captchas[id_] = [solution, None, now + delta, 0, access]
     CaptchaCount.count += 1
 
-    return jsonify({
-        'solution': solution,
-        'url': f'http://127.0.0.1:5000/api/cdn/{id_}'
-    })
+    return jsonify(
+        {"solution": solution, "url": f"http://127.0.0.1:5000/api/cdn/{id_}"}
+    )
 
 
-@app.route('/examples')
+@app.route("/examples")
 def examples():
     """API examples endpoint"""
     return render_template("examples.html")
 
 
-@app.route('/')
+@app.route("/")
 def home():
     """API home"""
     return render_template("index.html")
@@ -194,5 +184,5 @@ def not_found(e):
     return redirect("/")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
