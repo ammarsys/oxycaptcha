eeab0ea5f10224b4d5071ae07c3af1b6723c96bf
Ammar Hajeer <amarftw1@gmail.com>
2023-03-18 00:52:08 +0100

    nah

--- SETUP.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p id="This will clone the GitHub repository, install the requirements and run the webapp, to proceed type YES elsewise, close the window."
-git clone https://github.com/ammarsys/captchaAPI/
-cd captchaapi
-py -m pip install -r requirements.txt
-py app.py
