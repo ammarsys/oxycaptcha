aedf1780b4f31f22d9f6b55841976ce0ef7143b6
Ammar Hajeer <amarftw1@gmail.com>
2023-04-09 14:11:31 +0200

    circular import

--- utils/captcha_gen.py
+++ utils/captcha_gen.py
@@ -3,10 +3,9 @@
 import random
 import secrets
 import os
+import math
 
 from PIL import ImageDraw, Image, ImageFont
-
-from utils import noise
 
 
 FONTS_LOWER = [
@@ -21,6 +20,115 @@ FONTS_UPPER = [
     ImageFont.truetype(os.path.join("fonts", "upper", "TrainOne-Regular.ttf"), 60),
     ImageFont.truetype(os.path.join("fonts", "upper", "BebasNeue-Regular.ttf"), 60),
 ]
+
+
+def add_noise_lines(image: ImageDraw.ImageDraw) -> ImageDraw.ImageDraw:
+    """Add noise lines to an image."""
+    size = image.im.size  # type: ignore
+
+    for _ in range(secrets.randbelow(3)):
+        x = (-50, -50)
+        y = (size[0] + 10, secrets.choice(range(0, size[1] + 10)))
+
+        image.arc(x + y, 0, 360, fill="white")
+
+    return image
+
+
+def salt_and_pepper(image: Image.Image, probability: float) -> Image.Image:
+    """
+    Adds white pixels to a PIL image with a specified probability.
+
+    Args:
+        image (PIL.Image): The input image.
+        probability (float): The probability of adding a white pixel at each pixel location.
+
+    Returns:
+        PIL.Image: The output image with white pixels added.
+
+    """
+    output_image = Image.new(image.mode, image.size)
+
+    draw = ImageDraw.Draw(output_image)
+
+    for x in range(image.width):
+        for y in range(image.height):
+            random_number = secrets.choice(
+                (0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+            )
+
+            if random_number < probability:
+                draw.point((x, y), fill=(255, 255, 255))
+
+            else:
+                pixel = image.getpixel((x, y))
+                draw.point((x, y), fill=pixel)
+
+    return output_image
+
+
+def find_coeffs(angle):
+    if secrets.choice((0, 1)):
+        angle = math.radians(angle)
+    else:
+        angle = -math.radians(angle)
+
+    coeffs = [1, 0, 0, math.tan(angle), 1, 0, 0, 0, 1]
+
+    return coeffs
+
+
+def text_angled(
+    img,
+    xy: tuple[int, int],
+    text: str,
+    fill: tuple[int, int, int] | str,
+    font: ImageFont.FreeTypeFont,
+    rot_angle: int,
+    tilt_angle: int,
+    **kwargs
+):
+    """Wrapper around ImageDraw but you can specify an angle for the text
+
+    Args:
+        img (PIL.Image.Image): The 'Image' to draw the text on
+        rot_angle (int, optional): The angle for how much to rotate the text. Defaults to 0.
+        font (ImageFont.FreeTypeFont): font for the text
+        fill (tuple[int, int, int] | str): colour for the text
+        text (str): the text itself
+        xy (tuple[int, int]: coordinates for the text
+        tilt_angle (int): horizontal tilt for the text
+
+    Returns:
+        PIL.Image.Image: new 'Image' with the text on it
+
+    """
+    draw = ImageDraw.Draw(img)
+
+    text_width, text_height = draw.multiline_textsize(text, font=font)
+
+    # Create new image for the font
+    rotated_text_img = Image.new(
+        mode="RGBA", size=(text_width + 100, text_height + 100), color=(0, 0, 0, 0)
+    )
+    rotated_text_draw = ImageDraw.Draw(rotated_text_img)
+    rotated_text_draw.text((0, 0), text, fill=fill, font=font, **kwargs)
+
+    coeffs = find_coeffs(tilt_angle)
+    rotated_text_img = rotated_text_img.transform(
+        (
+            int(rotated_text_img.width + abs(coeffs[1] * rotated_text_img.height)),
+            rotated_text_img.height,
+        ),
+        Image.AFFINE,
+        coeffs,
+        Image.BICUBIC,
+    )
+    rotated_text_img = rotated_text_img.rotate(rot_angle, expand=True)
+
+    img.paste(rotated_text_img, xy, rotated_text_img)
+
+    return img
 
 
 def cap_gen(text: str) -> Image.Image:
@@ -56,7 +164,7 @@ def cap_gen(text: str) -> Image.Image:
 
         rot_angle = secrets.choice(range(-10, 10))
         tilt_angle = secrets.choice(range(10, 30))
-        img = noise.text_angled(
+        img = text_angled(
             img,
             cords,
             letter,
@@ -71,11 +179,11 @@ def cap_gen(text: str) -> Image.Image:
 
         text_positions.append(tuple(secrets.randbelow(10) + 15 + i for i in cords))
 
-    d = noise.add_noise_lines(ImageDraw.Draw(img))
+    d = add_noise_lines(ImageDraw.Draw(img))
 
     # Add a noise line relative to the text position
     value = secrets.randbelow(len(text_positions))
     for i in range(len(text_positions) - value):
         d.line((text_positions[i], text_positions[i + value]), fill=white, width=0)  # type: ignore
 
-    return noise.salt_and_pepper(img, probability=0.2)
+    return salt_and_pepper(img, probability=0.2)
